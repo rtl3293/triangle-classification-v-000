5dd5fd58d3d371ce0af32d34929c45d5cf2c0694
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-03 15:38:26 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,8 @@ class Triangle
   end
 
   def kind
-    if (@side1 + @side2 > @side3) && ((@side1 + @side3 > @side2) && (@side3 + @side2 > @side1)
+    if (@side1 + @side2 > @side3) && (@side1 + @side3 > @side2) && (@side3 + @side2 > @side1)
+      
   end
 end
 
