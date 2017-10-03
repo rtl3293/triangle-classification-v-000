38f272807ac3b753d54b3869dc01391790b59788
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-03 15:40:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,18 @@ class Triangle
 
   def kind
     if (@side1 + @side2 > @side3) && (@side1 + @side3 > @side2) && (@side3 + @side2 > @side1)
-      
+      if @side1 == @side2 || @side1 == @side3 || @side2 == @side3
+        if @side1 == @side2 && @side2 == @side3
+          :equilateral
+        else
+          :isosceles
+        end
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
   end
 end
 
